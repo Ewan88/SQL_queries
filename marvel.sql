eca1da3371e1c0f464f8ba487c72ea99a47ba4b7
Ewan88 <baileyewan@gmail.com>
2019-01-21 15:29:33 +0000

    added some comments

--- marvel.sql
+++ marvel.sql
@@ -48,22 +48,34 @@ INSERT INTO movies (title, year, show_time) VALUES ('Spider-Man: Homecoming', 20
 INSERT INTO movies (title, year, show_time) VALUES ('Thor: Ragnarok', 2017, '19:35');
 INSERT INTO movies (title, year, show_time) VALUES ('Black Panther', 2018, '16:50');
 
+-- 3.  Oops! Someone at CodeClan spelled Kimberly's name wrong! Change it to reflect the proper spelling ('Kimberly Clarke' should be 'Kimberly Clark').
 UPDATE people SET name = 'Kimberly Clark' WHERE name = 'Kimberly Clarke';
 
+-- 5.  The cinema is showing 'Batman Begins', but Batman is DC, not Marvel! Delete the entry from the 'movies' table.
 DELETE FROM movies WHERE title = 'Batman Begins';
 
+-- 6.  Create a new entry in the 'people' table with the name of one of the instructors.
 INSERT INTO people (name) VALUES ('John McCollum');
 
+-- 7.  John Smith has decided to hijack our movie evening, Remove him from the table of people.
 DELETE FROM people WHERE name = 'John Smith';
 
+-- 8.  The cinema has just heard that they will be holding an exclusive midnight showing of 'Avengers: Infinity War'!! Create a new entry in the 'movies' table to reflect this.
 INSERT INTO movies (title, year, show_time) VALUES ('Avengers: Infinity War', 2018, '00:00');
 
+-- 9.  The cinema would also like to make the Guardians movies a back to back feature. Find out the show time of "Guardians of the Galaxy" and set the show time of "Guardians of the Galaxy 2" to start two hours later.
 UPDATE movies SET show_time = '00:50' WHERE title = 'Guardians of the Galaxy 2';
 
+-- ## Extension
+--
+-- 1.  Research how to delete multiple entries from your table in a single command.
 DELETE FROM movies WHERE year IN (2008, 2010);
 
+-- 1.  Return ALL the data in the 'movies' table.
 SELECT * FROM movies;
 
+-- 2.  Return ONLY the name column from the 'people' table
 SELECT name FROM people;
 
+-- 4.  Return ONLY your name from the 'people' table.
 SELECT name FROM people WHERE name = 'Ewan Bailey';
